7dc39c51fc092a79aca7630ef6b58d12f0c8f64c
Risto Puolakainen <rpuolak@gmail.com>
2020-06-05 22:41:04 +0300

    Rewrite with proper arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,109 @@
 extern crate chrono;
+extern crate clap;
 
-use chrono::{Datelike, Local, NaiveTime, TimeZone, Timelike};
-use std::env;
+use chrono::{DateTime, Datelike, Local, NaiveTime, TimeZone, Timelike};
+use clap::{App, Arg, ArgMatches, SubCommand};
 use std::process;
 
-struct Params {
-    hours: u32,
-    mins: u32,
-}
+const DAYS: &str = "days";
+const HOURS: &str = "hours";
+const MINS: &str = "minutes";
+const BASE: &str = "NOT_SUBCMD";
 
-impl Params {
-    fn get() -> Params {
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() < 2 {
-            println!("Not enough arguments");
-            process::exit(1);
-        }
-
-        let hours_mins: NaiveTime = match NaiveTime::parse_from_str(&args[1], "%H:%M") {
-            Ok(val) => val,
-            Err(_err) => {
-                println!("error goddammit, input something in the format of HH:MM");
-                process::exit(1);
-            }
-        };
-        let hours: u32 = hours_mins.hour();
-        let mins: u32 = hours_mins.minute();
-
-        Params { hours, mins }
-    }
-}
-
-fn print_time(params: Params) {
-    let now = Local::now();
-
-    let target_time =
-        Local
-            .ymd(now.year(), now.month(), now.day())
-            .and_hms(params.hours, params.mins, 0);
-
-    let difference = now.signed_duration_since(target_time);
+fn print_time_difference(from: DateTime<Local>, to: DateTime<Local>) {
+    let difference = to.signed_duration_since(from);
 
     if difference.num_seconds() < 0 {
-        println!("No future values allowed here.");
+        println!("TODO: No future values allowed yet.");
         process::exit(1);
     }
 
-    //let start = Local.datetime_from_str("7:35", "%M:%S").unwrap();
     let hours = difference.num_hours();
     let mins = difference.num_minutes() % 60;
 
-    println!("{}h {}m", hours, mins);
+    println!("{:02}:{:02}", hours, mins);
+}
+
+fn fancy_print_epoch(cmd_name: &str) {
+    let epoch = Local::now().timestamp();
+    match cmd_name {
+        DAYS => println!("{}", epoch / 60 / 60 / 24),
+        HOURS => println!("{}", epoch / 60 / 60),
+        MINS => println!("{}", epoch / 60),
+        _ => println!("{}", epoch),
+    }
+}
+
+fn try_parse_time(arg: &str, now: &DateTime<Local>) -> DateTime<Local> {
+    match NaiveTime::parse_from_str(&arg, "%H:%M") {
+        Ok(val) => {
+            Local
+                .ymd(now.year(), now.month(), now.day())
+                .and_hms(val.hour(), val.minute(), 0)
+        }
+        Err(_err) => {
+            println!("TODO: %Y-%M-%D");
+            process::exit(1);
+        }
+    }
+}
+
+fn handle_args(name: &str, matches: &ArgMatches) {
+    let now = Local::now();
+
+    let from: DateTime<Local> = match matches.value_of("from") {
+        Some(val) => try_parse_time(val, &now),
+        None => {
+            fancy_print_epoch(name);
+            process::exit(1);
+        }
+    };
+
+    let to: DateTime<Local> = match matches.value_of("to") {
+        Some(val) => try_parse_time(val, &now),
+        None => now,
+    };
+
+    print_time_difference(from, to);
 }
 
 fn main() {
-    let params = Params::get();
+    let from: Arg = Arg::with_name("from")
+        .help("How much time has passed since date or time")
+        .required(false)
+        .index(1);
 
-    print_time(params);
+    let to: Arg = Arg::with_name("to")
+        .help("Use to set custom time range. Default is current datetime.")
+        .required(false)
+        .index(2);
+
+    let matches = App::new("since")
+        .about("Fetch time difference")
+        .arg(&from)
+        .arg(&to)
+        .subcommand(
+            SubCommand::with_name(DAYS)
+                .about("Return output in days")
+                .arg(&from)
+                .arg(&to),
+        )
+        .subcommand(
+            SubCommand::with_name(HOURS)
+                .about("Return output in hours")
+                .arg(&from)
+                .arg(&to),
+        )
+        .subcommand(
+            SubCommand::with_name(MINS)
+                .about("Return output in minutes")
+                .arg(&from)
+                .arg(&to),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        (name, Some(sub_matches)) => handle_args(name, sub_matches),
+        _ => handle_args(BASE, &matches),
+    }
 }
