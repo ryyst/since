1d3f6edf4bb12a59f06164aa09007c68f5216f98
Risto Puolakainen <rpuolak@gmail.com>
2020-06-15 12:51:58 +0300

    Refactor output & calculation functions for consistency
    - Calculation funcs now always return i64 (good for epoch)
    - Output funcs now always return strings
    
    In addition to cleaner code, this also makes unit testing of output
    formatting possible.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ const MINUTES: &str = "minutes";
 const SECONDS: &str = "seconds";
 const BASE: &str = "NOT_SUBCMD";
 
-fn calculate_month_diff(from: DateTime<Local>, to: DateTime<Local>) -> i32 {
+fn calculate_month_diff(from: DateTime<Local>, to: DateTime<Local>) -> i64 {
     // Individual typecasting is necessary to
     // a) compile at all
     // b) not panic from subtraction overflow
@@ -26,109 +26,89 @@ fn calculate_month_diff(from: DateTime<Local>, to: DateTime<Local>) -> i32 {
     let to_month = to.month() as i32;
 
     // TODO: Figure out if we want a more precise formula here.
-    ((from.year() - to.year()) * 12 + from_month - to_month).abs()
+    ((from.year() - to.year()) * 12 + from_month - to_month).abs() as i64
 }
 
-fn calculate_year_diff(from: DateTime<Local>, to: DateTime<Local>) -> i32 {
+fn calculate_year_diff(from: DateTime<Local>, to: DateTime<Local>) -> i64 {
     // TODO: Figure out if we want a more precise formula here.
-    (from.year() - to.year()).abs()
+    (from.year() - to.year()).abs() as i64
 }
 
 /// Print the time difference for shorthand use.
 ///
 /// Does some basic guessing on which format is the nicest for user to read.
-fn print_shorthand_format(from: DateTime<Local>, to: DateTime<Local>, difference: Duration) {
+fn get_shorthand_output(
+    from: DateTime<Local>,
+    to: DateTime<Local>,
+    difference: Duration,
+) -> String {
     // TODO: Clean this up and add "and"-clauses to every option, like in hours/minutes now.
     let days = difference.num_days().abs();
     match days {
         // About two months to about two years
-        63..=730 => {
-            println!("{} months", calculate_month_diff(from, to));
-        }
+        63..=730 => format!("{} months", calculate_month_diff(from, to)),
         // Two days to about two months
-        2..=62 => {
-            println!("{} days", difference.num_days().abs());
-        }
+        2..=62 => format!("{} days", difference.num_days().abs()),
         0..=1 => {
-            let hours = difference.num_hours();
-            let mins = difference.num_minutes() % 60;
+            let hours = difference.num_hours().abs();
+            let mins = (difference.num_minutes() % 60).abs();
 
             // TODO: Seconds
             if hours == 0 {
-                println!("{} minutes", mins.abs());
+                format!("{} minutes", mins)
             } else {
-                println!("{} hours and {} minutes", hours.abs(), mins.abs());
+                format!("{} hours and {} minutes", hours, mins)
             }
         }
-        _ => {
-            println!("{} years", calculate_year_diff(from, to));
-        }
+        _ => format!("{} years", calculate_year_diff(from, to)),
     }
 }
 
-/// Print the requested `from` and `to` arguments according to the chosen subcommand.
+/// Return the requested time difference filtered according to the chosen subcommand.
 ///
 /// If no subcommand is chosen, guess which is the best format for humans to read
 /// for the given time range.
-fn print_time_difference(from: DateTime<Local>, to: DateTime<Local>, subcmd: &str) {
+fn get_output(from: DateTime<Local>, to: DateTime<Local>, subcmd: &str) -> String {
     let difference = to.signed_duration_since(from);
+
     // NOTE:
     // All values are printed in absolutes, as to not show negative number for values in
     // future. While this is breaking the semantics of `since` a bit, we'll allow it for
     // better usability. You could basically just symlink `since` -> `until`.
-
     match subcmd {
-        YEARS => {
-            println!("{}", calculate_year_diff(from, to));
-        }
-        MONTHS => {
-            println!("{}", calculate_month_diff(from, to));
-        }
-        WEEKS => {
-            println!("{}", difference.num_weeks().abs());
-        }
-        DAYS => {
-            println!("{}", difference.num_days().abs());
-        }
-        HOURS => {
-            println!("{}", difference.num_hours().abs());
-        }
-        MINUTES => {
-            println!("{}", difference.num_minutes().abs());
-        }
-        SECONDS => {
-            println!("{}", difference.num_seconds().abs());
-        }
-        _ => {
-            print_shorthand_format(from, to, difference);
-        }
+        YEARS => calculate_year_diff(from, to).to_string(),
+        MONTHS => calculate_month_diff(from, to).to_string(),
+        WEEKS => difference.num_weeks().abs().to_string(),
+        DAYS => difference.num_days().abs().to_string(),
+        HOURS => difference.num_hours().abs().to_string(),
+        MINUTES => difference.num_minutes().abs().to_string(),
+        SECONDS => difference.num_seconds().abs().to_string(),
+        _ => get_shorthand_output(from, to, difference),
     }
 }
 
-/// Print the UNIX timestamp according to the chosen subcommand.
-fn print_formatted_epoch(subcmd: &str, now: DateTime<Local>) {
+/// Return the UNIX timestamp filtered according to the chosen subcommand.
+fn get_epoch_output(subcmd: &str, now: DateTime<Local>) -> String {
     let epoch = Local::now().timestamp();
     let epoch_date = Local.ymd(1970, 1, 1).and_hms(0, 0, 0);
 
-    match subcmd {
+    let output: i64 = match subcmd {
         // Epoch days are always statically 86400 seconds long.
         // Thus the following calculations are just "close enough" approximations
-        YEARS => {
-            println!("{}", (1970 - now.year()).abs());
-        }
-        MONTHS => {
-            println!("{}", calculate_month_diff(epoch_date, now));
-        }
+        YEARS => (1970 - now.year()).abs() as i64,
+        MONTHS => calculate_month_diff(epoch_date, now),
         WEEKS => {
             let difference = epoch_date.signed_duration_since(now);
-            println!("{}", difference.num_weeks().abs())
+            difference.num_weeks().abs()
         }
         // ...and these naive calculations should actually be 100% correct
-        DAYS => println!("{}", epoch / 60 / 60 / 24),
-        HOURS => println!("{}", epoch / 60 / 60),
-        MINUTES => println!("{}", epoch / 60),
-        _ => println!("{}", epoch),
-    }
+        DAYS => epoch / 60 / 60 / 24,
+        HOURS => epoch / 60 / 60,
+        MINUTES => epoch / 60,
+        _ => epoch,
+    };
+
+    output.to_string()
 }
 
 fn handle_args(subcmd: &str, matches: &ArgMatches) {
@@ -143,7 +123,7 @@ fn handle_args(subcmd: &str, matches: &ArgMatches) {
             }
         },
         None => {
-            print_formatted_epoch(subcmd, now);
+            println!("{}", get_epoch_output(subcmd, now));
             process::exit(0);
         }
     };
@@ -159,7 +139,7 @@ fn handle_args(subcmd: &str, matches: &ArgMatches) {
         None => now,
     };
 
-    print_time_difference(from, to, subcmd);
+    println!("{}", get_output(from, to, subcmd));
 }
 
 fn main() {
@@ -240,5 +220,5 @@ All values are generally rounded down.";
     match matches.subcommand() {
         (subcmd, Some(sub_matches)) => handle_args(subcmd, sub_matches),
         _ => handle_args(BASE, &matches),
-    }
+    };
 }
